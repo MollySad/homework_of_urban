0afcdf6c75ecb272fc8a3a0d32811c0d40dcd3d4
MollySad <lane3154@gmail.com>
2024-07-29 22:19:22 +0700

    готовая работа

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input('Что-то напишите:')
+
+print('В вашей строке:', len(my_string), 'символа(ов)')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
