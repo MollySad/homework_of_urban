2f8e00e29560658d1f72e007a2135bdce80a428b
MollySad <lane3154@gmail.com>
2024-08-12 12:00:10 +0700

    задание "Рассылка писем v2"

--- module3_2.py
+++ module3_2.py
@@ -2,7 +2,7 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
     if '@' not in recipient or not (
             recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net')):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
-    if '@' not in sender or not (
+    elif '@' not in sender or not (
             sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net')):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
     elif sender == recipient:
