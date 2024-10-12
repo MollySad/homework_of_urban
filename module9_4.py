61894d04ddeb83d97176c5596f28da3dbe960257
MollySad <lane3154@gmail.com>
2024-10-12 20:34:17 +0700

    задание "Функциональное разнообразие"

---
+++ module9_4.py
@@ -0,0 +1,39 @@
+import random
+from random import choice
+
+# lambda
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
+
+
+# замыкание
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        file = open(file_name, 'a', encoding='utf-8')
+        for i in data_set:
+            file.write(f'{i}\n')
+        file.close()
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# метод __call__
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return random.choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
