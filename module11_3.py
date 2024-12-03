718fd9980b2ef72bc2045dcd00c834b93b4414af
MollySad <lane3154@gmail.com>
2024-12-03 20:18:18 +0700

    задание "Интроспекция_2"

--- module11_3.py
+++ module11_3.py
@@ -4,12 +4,15 @@ def introspection_info(obj):
     intro = {}
 
     intro['type'] = type(obj).__name__
-    intro['attributes'] = dir(obj)
+    attr_list = []
     method_list = []
     for attr_name in dir(obj):
         attr = getattr(obj, attr_name)
         if callable(attr):
             method_list.append(attr_name)
+        else:
+            attr_list.append(attr_name)
+    intro['attributes'] = attr_list
     intro['methods'] = method_list
     intro['module'] = inspect.getmodule(obj)
 
